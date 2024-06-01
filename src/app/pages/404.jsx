1e74f18053ab07138d526d656eccdc4ed981a048
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-06-01 17:53:29 +0100

    Styles the button on the 404 page and also configured it to reroute users to their previous page

--- src/app/pages/404.jsx
+++ src/app/pages/404.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "../styles/notfound.css";
 import logo from "../../app/assets/404.jpeg";
+import styles from "./pages.module.css"
 
 function Page() {
+  const goBack = () => {
+    window.history.back()
+  };
+
   return (
-    <div className='notfound'>
-      {<img src={logo} alt='logo' />}
-      <a href='/' target='_blank' rel='noopener noreferrer'>
-        Back to home
-      </a>
+    <div className="notfound">
+      {<img src={logo} alt="logo" />}
+      <button
+        onClick={goBack}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.container}
+      >
+        Go back...
+      </button>
     </div>
   );
 }
